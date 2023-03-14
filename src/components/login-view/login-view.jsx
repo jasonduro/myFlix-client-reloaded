d3f79ab090e0887956799ede44ab0d46d246fbd7
Jason <product@jasonlamanque.com>
2023-03-14 20:17:23 +0100

    update login

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -15,49 +15,52 @@ export const LoginView = () => {
 
 		fetch("https://myflix-app-jl.herokuapp.com/login", {
 			method: "POST",
-      headers: {
-        "Content-Type": "application/json"
-      },
-			body: JSON.stringify(data)
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify(data),
 		})
-      .then((response) => response.json())
-      .then((data) => {
-        console.log("Login response: ", data);
-			if (data.user) {
-				onLoggedIn(data.user, data.token);
-			} else {
-				alert("No such user");
-			}
-		})
-      .catch((e) => {
-        alert("Error logging in");
-	});
+			.then((response) => response.json())
+			.then((data) => {
+				console.log("Login response: ", data);
+				if (data.user) {
+					localStorage.setItem("token", data.token);
+					localStorage.setItem("user", JSON.stringify(data.user));
+					onLoggedIn(data.user, data.token);
+				} else {
+					alert("No such user");
+				}
+			})
+			.catch((e) => {
+				alert("Error logging in");
+			});
 
-	return (
-		<div>
-			<h1>MyFlix Login</h1>
-			<form onSubmit={handleSubmit}>
-				<label>
-					Username:
-					<input
-						type='text'
-						value={username}
-						onChange={(e) => setUsername(e.target.value)}
-						required
-						minLength={3}
-					/>
-				</label>
-				<label>
-					Password:
-					<input
-						type='password'
-						value={password}
-						onChange={(e) => setPassword(e.target.value)}
-						required
-					/>
-				</label>
-				<button type='submit'>Submit</button>
-			</form>
-		</div>
-	);
+		return (
+			<div>
+				<h1>MyFlix Login</h1>
+				<form onSubmit={handleSubmit}>
+					<label>
+						Username:
+						<input
+							type='text'
+							value={username}
+							onChange={(e) => setUsername(e.target.value)}
+							required
+							minLength={3}
+						/>
+					</label>
+					<label>
+						Password:
+						<input
+							type='password'
+							value={password}
+							onChange={(e) => setPassword(e.target.value)}
+							required
+						/>
+					</label>
+					<button type='submit'>Submit</button>
+				</form>
+			</div>
+		);
+	};
 };
