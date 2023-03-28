0a8b2a76bdef04d0d0b1a5adb2e5da156f0e815c
Jason <product@jasonlamanque.com>
2023-03-28 15:25:43 +0200

    update main-view file
    removed props from loginview and movieview. added movielist

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,5 +1,3 @@
-// This is the main view of the app, which will be rendered by the main component (index.jsx). It will display a list of movies. When a user clicks on a movie, the app will display information about that movie, and a button that will return the user to the main view. This component will be a "smart" component because it will maintain state, in this case the list of movies and the selected movie.
-
 import React from "react";
 import { useState, useEffect } from "react";
 import { MovieCard } from "../movie-card/movie-card";
@@ -12,12 +10,20 @@ import Col from "react-bootstrap/Col";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { NavigationBar } from "../navigation-bar/navigation-bar";
 
+import { useSelector, useDispatch } from "react-redux";
+import { setMovies } from "../../redux/reducers/movies";
+import { MoviesList } from "../movies-list/movies-list";
+
 const MainView = () => {
-	const storedUser = localStorage.getItem("user");
-	const storedToken = localStorage.getItem("token");
-	const [movies, setMovies] = useState([]);
-	const [user, setUser] = useState(null);
-	const [token, setToken] = useState(null);
+	const movies = useSelector((state) => state.movies.list);
+	const user = useSelector(
+		(state) => state.user.user || JSON.parse(localStorage.getItem("user"))
+	);
+	const token = useSelector(
+		(state) => state.token.token || localStorage.getItem("token")
+	);
+
+	const dispatch = useDispatch();
 
 	useEffect(() => {
 		if (!token) return;
@@ -25,13 +31,12 @@ const MainView = () => {
 			headers: { Authorization: `Bearer ${token}` },
 		})
 			.then((response) => response.json())
-			.then((data) => setMovies(data));
+			.then((data) => dispatch(setMovies(data)));
 	}, [token]);
 
 	return (
 		<BrowserRouter>
 			<NavigationBar
-				user={user}
 				onLoggedOut={() => {
 					setUser(null);
 					setToken(null);
@@ -63,12 +68,7 @@ const MainView = () => {
 									<Navigate to='/' />
 								) : (
 									<Col xs={12} md={6}>
-										<LoginView
-											onLoggedIn={(user, token) => {
-												setUser(user);
-												setToken(token);
-											}}
-										/>
+										<LoginView />
 									</Col>
 								)}
 							</>
@@ -85,7 +85,7 @@ const MainView = () => {
 									<Col>The list is empty!</Col>
 								) : (
 									<Col xs={12} style={{ border: "1px solid black" }}>
-										<MovieView movies={movies} user={user} token={token} />
+										<MovieView />
 									</Col>
 								)}
 							</>
@@ -95,28 +95,7 @@ const MainView = () => {
 					<Route
 						path='/'
 						element={
-							<>
-								{!user ? (
-									<Navigate to='/login' replace />
-								) : movies.length === 0 ? (
-									<Col>The list is empty!</Col>
-								) : (
-									<>
-										{movies.map((movie) => (
-											<Col
-												className='mb-4'
-												key={movie._id}
-												xs={12}
-												sm={6}
-												md={4}
-												lg={3}
-											>
-												<MovieCard movie={movie} user={user} token={token} />
-											</Col>
-										))}
-									</>
-								)}
-							</>
+							<>{!user ? <Navigate to='/login' replace /> : <MoviesList />}</>
 						}
 					/>
 					<Route
